2eadb582d759b3255bc7235d84074652291c8bda
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2023-04-05 14:47:33 -0500

    Ejercicio1

---
+++ src/main/java/com/example/demo/Ejercicio1Application.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.modelo.Persona;
+
+@SpringBootApplication
+public class Ejercicio1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Persona persona1 = new Persona();
+		persona1.setNombre("juan");
+		persona1.setEdad(27);
+		System.out.println("El nombre de la persona es:" + persona1.getNombre());
+		System.out.println("La edad de " + persona1.getNombre() + " es:" + persona1.getEdad());
+		System.out.println(persona1);
+	}
+}
---
+++ src/main/java/com/example/demo/modelo/Persona.java
@@ -0,0 +1,30 @@
+package com.example.demo.modelo;
+
+public class Persona {
+	private String nombre;
+	private int edad;
+
+	// setters and getters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+	}
+
+	
+}
